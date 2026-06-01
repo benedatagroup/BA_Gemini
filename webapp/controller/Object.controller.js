d82d266da91537fed1500044ac9d2001bb76fe4c
Benedikt Maurer <benedikt.maurer@inn2.de>
2026-06-01 13:17:36 +0000

    TaxItems done

--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -22,8 +22,22 @@ sap.ui.define([
             
             var sInvoiceId = oEvent.getParameter("arguments").InvoiceId;
             this.getView().bindElement({
-                path: "/Invoices('" + sInvoiceId + "')"
+                path: "/Invoices('" + sInvoiceId + "')",
+                events: {
+                    dataReceived: function () {
+                        var oTable = this.getView().byId("invoiceItemsTable");
+                        var oBinding = oTable.getBinding("items");
+                        if (oBinding && !this._bBindingAttached) {
+                            oBinding.attachDataReceived(this._onTableDataReceived, this);
+                            this._bBindingAttached = true;
+                        }
+                    }.bind(this)
+                }
             });
+        },
+
+        _onTableDataReceived: function () {
+            this._recalculateTotals();
         },
 
         onNavBack: function () {
@@ -57,6 +71,7 @@ sap.ui.define([
         onAddItem: function () {
             var oView = this.getView();
             var oContext = oView.getBindingContext();
+            var oModel = oContext.getModel();
             var sInvoiceId = oContext.getProperty("InvoiceId");
             
             var sItemId = "ITEM_" + new Date().getTime();
@@ -74,13 +89,16 @@ sap.ui.define([
                 GrossAmount: "0.00"
             };
 
-            var oTable = oView.byId("invoiceItemsTable");
-            var oBinding = oTable.getBinding("items");
-            
-            // Add the new row directly to the table binding at the end
-            oBinding.create(oNewItemData, true);
-            
-            this._recalculateTotals();
+            try {
+                oModel.createEntry("InvoiceItems", {
+                    context: oContext,
+                    properties: oNewItemData
+                });
+                
+                this._recalculateTotals();
+            } catch (e) {
+                MessageBox.error("Fehler beim Hinzufügen der Position: " + e.message);
+            }
         },
 
         onItemChange: function (oEvent) {
@@ -118,14 +136,123 @@ sap.ui.define([
             
             var fTotalNet = 0;
             var fTotalGross = 0;
+            var oTaxGroups = {};
 
             aContexts.forEach(function (oItemContext) {
-                fTotalNet += parseFloat(oItemContext.getProperty("NetAmount") || 0);
-                fTotalGross += parseFloat(oItemContext.getProperty("GrossAmount") || 0);
+                if (!oItemContext) {
+                    return;
+                }
+                var fNet = parseFloat(oItemContext.getProperty("NetAmount") || 0);
+                var fGross = parseFloat(oItemContext.getProperty("GrossAmount") || 0);
+                var sTaxRate = oItemContext.getProperty("TaxRate");
+                var fTaxRate = parseFloat(sTaxRate || 0);
+                
+                fTotalNet += fNet;
+                fTotalGross += fGross;
+                
+                if (sTaxRate !== undefined && sTaxRate !== null && sTaxRate !== "") {
+                    var sKey = fTaxRate.toFixed(2);
+                    if (!oTaxGroups[sKey]) {
+                        oTaxGroups[sKey] = { taxBase: 0, taxAmount: 0 };
+                    }
+                    oTaxGroups[sKey].taxBase += fNet;
+                }
+            });
+            
+            Object.keys(oTaxGroups).forEach(function(sRateKey) {
+                var fTaxRate = parseFloat(sRateKey);
+                oTaxGroups[sRateKey].taxAmount = oTaxGroups[sRateKey].taxBase * (fTaxRate / 100);
             });
             
             oModel.setProperty("NetAmount", fTotalNet.toFixed(2), oContext);
             oModel.setProperty("GrossAmount", fTotalGross.toFixed(2), oContext);
+
+            var oTaxTable = oView.byId("taxItemsTable");
+            if (oTaxTable) {
+                var oTaxBinding = oTaxTable.getBinding("items");
+                if (oTaxBinding) {
+                    var aTaxContexts = oTaxBinding.getContexts();
+                    var oProcessedRates = {};
+                    
+                    aTaxContexts.forEach(function(oTaxContext) {
+                        if (!oTaxContext) {
+                            return;
+                        }
+                        var sCurrentRateKey = parseFloat(oTaxContext.getProperty("TaxRate") || 0).toFixed(2);
+                        if (oTaxGroups[sCurrentRateKey]) {
+                            oModel.setProperty("TaxBase", oTaxGroups[sCurrentRateKey].taxBase.toFixed(2), oTaxContext);
+                            oModel.setProperty("TaxAmount", oTaxGroups[sCurrentRateKey].taxAmount.toFixed(2), oTaxContext);
+                            oProcessedRates[sCurrentRateKey] = true;
+                        } else {
+                            var sTaxPath = oTaxContext.getPath();
+                            var bIsTaxTransient = oTaxContext.bCreated || (oTaxContext.isTransient && oTaxContext.isTransient()) || sTaxPath.indexOf("id-") !== -1 || sTaxPath.indexOf("TAX_") !== -1;
+                            if (bIsTaxTransient) {
+                                if (oModel.deleteCreatedEntry) {
+                                    oModel.deleteCreatedEntry(oTaxContext);
+                                } else {
+                                    oModel.resetChanges([sTaxPath]);
+                                }
+                            } else {
+                                oModel.remove(sTaxPath, {
+                                    success: function() {},
+                                    error: function() {}
+                                });
+                            }
+                        }
+                    });
+                    
+                    Object.keys(oTaxGroups).forEach(function(sRateKeyToCreate) {
+                        if (!oProcessedRates[sRateKeyToCreate]) {
+                            var sTaxItemId = "TAX_" + new Date().getTime() + "_" + Math.floor(Math.random() * 1000);
+                            try {
+                                oModel.createEntry("TaxItems", {
+                                    context: oContext,
+                                    properties: {
+                                        TaxItemId: sTaxItemId,
+                                        InvoiceId: oContext.getProperty("InvoiceId"),
+                                        TaxRate: parseFloat(sRateKeyToCreate).toFixed(2),
+                                        TaxBase: oTaxGroups[sRateKeyToCreate].taxBase.toFixed(2),
+                                        TaxAmount: oTaxGroups[sRateKeyToCreate].taxAmount.toFixed(2)
+                                    }
+                                });
+                            } catch(e) {
+                                console.error("Error creating TaxItem:", e);
+                            }
+                        }
+                    });
+                }
+            }
+        },
+
+        onDeleteItem: function (oEvent) {
+            var oItem = oEvent.getParameter("listItem");
+            var oItemContext = oItem.getBindingContext();
+            var oModel = oItemContext.getModel();
+            
+            var sPath = oItemContext.getPath();
+            var bIsTransient = oItemContext.bCreated || 
+                               (oItemContext.isTransient && oItemContext.isTransient()) || 
+                               sPath.indexOf("id-") !== -1 || 
+                               sPath.indexOf("TAX_") !== -1 || 
+                               sPath.indexOf("ITEM_") !== -1;
+
+            if (bIsTransient) {
+                if (oModel.deleteCreatedEntry) {
+                    oModel.deleteCreatedEntry(oItemContext);
+                } else {
+                    oModel.resetChanges([sPath]);
+                }
+                this._recalculateTotals();
+            } else {
+                oModel.remove(sPath, {
+                    success: function() {
+                        // Binding change will trigger dataReceived which recalculates totals
+                    }.bind(this),
+                    error: function() {
+                        MessageBox.error("Fehler beim Löschen der Position.");
+                    }
+                });
+            }
         },
 
         onSave: function () {
--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -23,6 +23,23 @@ sap.ui.define([
 			var sPath = sap.ui.require.toUrl("bagemini/localService");
 			oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
 
+			var aRequests = oMockServer.getRequests();
+			aRequests.forEach(function (oRequest) {
+				if (oRequest.method === "GET") {
+					var fnOriginalHandler = oRequest.response;
+					oRequest.response = function (oXhr) {
+						if (oXhr.url && oXhr.url.match(/(?:%24|\$)filter=[^&]*(?:not%28|not\()/)) {
+							oXhr.url = oXhr.url.replace(/[?&](?:%24|\$)filter=[^&]*(?:not%28|not\()[^&]*/g, "");
+							if (oXhr.url.indexOf("?") === -1 && oXhr.url.indexOf("&") !== -1) {
+								oXhr.url = oXhr.url.replace("&", "?");
+							}
+						}
+						return fnOriginalHandler.apply(this, arguments);
+					};
+				}
+			});
+			oMockServer.setRequests(aRequests);
+
 			// start
 			oMockServer.start();
 		}
