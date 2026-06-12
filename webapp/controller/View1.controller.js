a5f59a9ddc6a2f632929fbb6f20cbf32e0da607e
Benedikt Maurer <benedikt.maurer@inn2.de>
2026-06-12 11:28:10 +0000

    Create invoice done

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,124 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], (Controller, Filter, FilterOperator) => {
+    "sap/ui/model/FilterOperator",
+    "sap/ui/core/Fragment",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
+], (Controller, Filter, FilterOperator, Fragment, MessageBox, MessageToast) => {
     "use strict";
 
     return Controller.extend("bagemini.controller.View1", {
         onInit() {
+        },
+
+        onCreateInvoicePress: function () {
+            var oView = this.getView();
+            if (!this.byId("createInvoiceDialog")) {
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "bagemini.view.CreateInvoiceDialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    this._resetCreateForm();
+                    oDialog.open();
+                }.bind(this));
+            } else {
+                this._resetCreateForm();
+                this.byId("createInvoiceDialog").open();
+            }
+        },
+
+        _resetCreateForm: function() {
+            this.byId("newInvoiceNumber").setValue("");
+            this.byId("newInvoiceNumber").setValueState("None");
+            this.byId("newVendorName").setValue("");
+            this.byId("newVendorName").setValueState("None");
+            this.byId("newInvoiceDate").setDateValue(null);
+            this.byId("newInvoiceDate").setValueState("None");
+            this.byId("newCurrency").setValue("");
+            this.byId("newCurrency").setValueState("None");
+            this.byId("newDueDate").setDateValue(null);
+        },
+
+        onCreateInvoiceSave: function () {
+            var oInvoiceNumber = this.byId("newInvoiceNumber");
+            var oVendorName = this.byId("newVendorName");
+            var oInvoiceDate = this.byId("newInvoiceDate");
+            var oCurrency = this.byId("newCurrency");
+            var oDueDate = this.byId("newDueDate");
+
+            var bValid = true;
+
+            if (!oInvoiceNumber.getValue()) {
+                bValid = false;
+                oInvoiceNumber.setValueState("Error");
+            } else {
+                oInvoiceNumber.setValueState("None");
+            }
+
+            if (!oVendorName.getValue()) {
+                bValid = false;
+                oVendorName.setValueState("Error");
+            } else {
+                oVendorName.setValueState("None");
+            }
+
+            if (!oInvoiceDate.getDateValue()) {
+                bValid = false;
+                oInvoiceDate.setValueState("Error");
+            } else {
+                oInvoiceDate.setValueState("None");
+            }
+
+            if (!oCurrency.getValue()) {
+                bValid = false;
+                oCurrency.setValueState("Error");
+            } else {
+                oCurrency.setValueState("None");
+            }
+
+            if (!bValid) {
+                var sErrorMsg = this.getView().getModel("i18n").getResourceBundle().getText("errorMandatoryFields");
+                MessageBox.error(sErrorMsg);
+                return;
+            }
+
+            var sInvoiceId = "INV" + Date.now().toString();
+
+            var oPayload = {
+                InvoiceId: sInvoiceId,
+                InvoiceNumber: oInvoiceNumber.getValue(),
+                VendorName: oVendorName.getValue(),
+                InvoiceDate: oInvoiceDate.getDateValue(),
+                Currency: oCurrency.getValue().toUpperCase(),
+                Status: "Draft",
+                NetAmount: "0.00",
+                GrossAmount: "0.00"
+            };
+
+            var dDueDate = oDueDate.getDateValue();
+            if (dDueDate) {
+                oPayload.DueDate = dDueDate;
+            }
+
+            var oModel = this.getView().getModel();
+            oModel.create("/Invoices", oPayload, {
+                success: function () {
+                    var sSuccessMsg = this.getView().getModel("i18n").getResourceBundle().getText("btnSave") + " erfolgreich";
+                    MessageToast.show(sSuccessMsg);
+                    this.byId("createInvoiceDialog").close();
+                    this.byId("invoiceTable").getBinding("items").refresh();
+                }.bind(this),
+                error: function () {
+                    MessageBox.error("Fehler beim Erstellen der Rechnung");
+                }
+            });
+        },
+
+        onCreateInvoiceCancel: function () {
+            this.byId("createInvoiceDialog").close();
         },
 
         onSearch: function () {
